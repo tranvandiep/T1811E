5696ca1c477ad43268ec8e794d265f5c165f26e5
LexNguyen <linhnguyenmanh2681993@gmail.com>
2019-05-27 21:02:22 +0700

    update

---
+++ users/Thonas_Nguyen(Linhnmth1810013)/T1811E_Java_Practice/src/BT52_Bai_tap_on_luyen_lap_trinh_OOP/Book.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BT52_Bai_tap_on_luyen_lap_trinh_OOP;
+
+/**
+ *
+ * @author linhn
+ */
+public class Book {
+    private String bookName;
+    private String bookAuthor;
+    private String producer;
+    private int yearPublishing;
+    private float price;
+
+    public Book() {
+    }
+
+    public Book(String bookName, String bookAuthor, String producer, int yearPublishing, float price) {
+        this.bookName = bookName;
+        this.bookAuthor = bookAuthor;
+        this.producer = producer;
+        this.yearPublishing = yearPublishing;
+        this.price = price;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public int getYearPublishing() {
+        return yearPublishing;
+    }
+
+    public void setYearPublishing(int yearPublishing) {
+        this.yearPublishing = yearPublishing;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+    
+    
+}
---
+++ users/Thonas_Nguyen(Linhnmth1810013)/T1811E_Java_Practice/src/BT7_On_luyen_ve_OOP_task_1/main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BT7_On_luyen_ve_OOP_task_1;
+
+/**
+ *
+ * @author linhn
+ */
+public class main {
+    public static void main(String[] args) {
+        
+    }
+}
