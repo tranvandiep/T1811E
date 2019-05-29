5b4bc13f7193a2f89d55532f419d5c7dc5ae8f99
ThanhDuy1310 <45651866+ThanhDuy1310@users.noreply.github.com>
2019-05-29 20:42:33 +0700

    bai tap

---
+++ NetBeansProjects/Aptech/src/Bai52/AptechBook.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bai52;
+import java.util.Scanner;
+/**
+ *
+ * @author giangduy
+ */
+public class AptechBook extends Book{
+    private String language;
+
+    private int semester;
+
+    public AptechBook() {
+    }
+
+    public AptechBook(String language, int semester, String bookName, String bookAuthor, String producer, int yearPublishing, float price) {
+        super(bookName, bookAuthor, producer, yearPublishing, price);
+        this.language = language;
+        this.semester = semester;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+    @Override
+    public void input() {
+        super.input(); //To change body of generated methods, choose Tools | Templates.
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Nhap ngon ngu : ");
+        language = input.nextLine();
+        
+        System.out.println("Nhap ky hoc : ");
+        semester = Integer.parseInt(input.nextLine());
+    }
+
+    @Override
+    public void display() {
+        System.out.printf("Ten sach : %s, "
+                + "tac gia : %s, nxb : %s, "
+                + "nam xuat ban : %d, "
+                + "gia ban : %f, "
+                + "ngon ngu : %s, "
+                + "ky hoc : %d", 
+                getBookName(), getBookAuthor(), 
+                getProducer(), getYearPublishing(), 
+                getPrice(), language, semester);
+    }
+}
