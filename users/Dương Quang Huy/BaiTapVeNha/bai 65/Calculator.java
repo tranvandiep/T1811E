6ccf44bcf7831f3e6be41140d9216d1df1633457
= <=>
2019-05-30 21:08:03 +0700

    updata

---
+++ users/Dương Quang Huy/BaiTapVeNha/bai 65/Calculator.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Maytinh;
+
+/**
+ *
+ * @author DuongQuangHuy
+ */
+public class Calculator{
+    public int Cong(int a ,int b){
+        return a + b;
+    };
+    public int Tru(int a, int b){
+        return a - b;
+    };
+     public int Nhan(int a, int b){
+        return a * b;
+    };
+      public int Chia(int a, int b){
+        return a / b;
+    };
+  
+}
