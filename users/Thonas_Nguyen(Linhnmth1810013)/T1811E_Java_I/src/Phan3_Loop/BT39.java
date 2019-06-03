beba4cfa0291f2c4ec1e4cb4d5c50e97277155dc
LexNguyen <linhnguyenmanh2681993@gmail.com>
2019-06-03 15:03:19 +0700

    update practice

---
+++ users/Thonas_Nguyen(Linhnmth1810013)/T1811E_Java_I/src/Phan3_Loop/BT39.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Phan3_Loop;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author linhn
+ */
+public class BT39 {
+    public static void main(String[] args) {
+/*Practice 1:
+        Scanner input = new Scanner(System.in);
+        int sum = 0;
+        int[] arr = new int[10];
+        for(int i = 0; i < 10; i++){
+           System.out.println("Element " + i + "is:");
+           arr[i] = Integer.parseInt(input.nextLine());
+           sum = arr[i] + sum;
+        }
+        System.out.println("Sum of array is:" + sum);
+-------------------------------------------------------*/
+/*Practice 2:
+        int n;
+        Scanner input = new Scanner(System.in);
+        System.out.printf("input array number: ");
+        n = Integer.parseInt(input.nextLine());
+        int sum = 0;
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            System.out.println("Element " + i + " is:");
+            arr[i] = Integer.parseInt(input.nextLine());
+            if((arr[i]%3) == 0 ){
+                sum = arr[i] + sum;
+            }  
+        }
+        System.out.println("Sum of array can devine 3: " + sum);
+--------------------------------------------------------*/
+/*Practice 3:
+        int n;
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Input number :");
+        n = Integer.parseInt(input.nextLine());
+        for(int i = 0; i<n; i++){
+            System.out.println();
+            for(int j =0; j<i+15; j++){
+                System.out.printf("*");
+            }
+        }
+--------------------------------------------------------*/
+        int max;
+        Scanner input = new Scanner(System.in);
+        
+    }
+}
