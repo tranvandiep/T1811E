7a76ec1ea26619ff37613f71b121998d11523037
cuongus18 <cuongungsy@gmail.com>
2019-06-05 19:49:51 +0700

    Assignment-KT 60 phut - fix

--- users/cuongus/HomeWork/src/bt522/Test.java
+++ users/cuongus/HomeWork/src/bt522/Test.java
@@ -7,6 +7,7 @@ package bt522;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 
 /**
@@ -14,16 +15,43 @@ import java.util.List;
  * @author anthony_jsc
  */
 public class Test {
-    public static void main(String[] args) {
-        List<IInfor> list = new ArrayList<>();
-        People people = new People();
-        Car car = new Car();
+    static List<IInfor> list = new ArrayList<>();
+    static People people = new People();
+    static Car car = new Car();
+    public static void main(String[] args) { 
         
-        people.input();
-        car.input();
+//        people.input();
+//        car.input();
+//        
+//        people.showInfor();
+//        car.showInfor();
+
+//        list.add(car);
+//        list.add(car);
+//        list.add(people);
+//        list.add(people);
+//        
+//        people.input();
+//        people.input();
+//        car.input();
+        int n;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nhap so luong doi tuong: ");
+        n = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < n; i++) {
+            System.out.println("Nhap thong tin nguoi : " + (i+1));
+            people.input();
+            System.out.println("Nhap thong xe : " + (i+1));
+            car.input();
+            
+            list.add(people);
+            list.add(car);
+        }
         
-        people.showInfor();
-        car.showInfor();
+        for (int i = 0; i < list.size(); i++) {
+            people.showInfor();
+            car.showInfor();
+        }
     }
    
 }
