33b2ebcab85baa30072d39a4b57ce1eca53fb39d
Le Quoc Tuan <letuan311299@gmail.com>
2019-06-07 19:28:26 +0700

    up bai tap

---
+++ users/LeQuocTuan/assignment60Phut/People.java
@@ -0,0 +1,73 @@
+﻿/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package APTECH.assignment60Phut;
+import java.util.Scanner;
+
+/**
+ *
+ * @author LQT
+ */
+public class People implements IInfor{
+    Scanner z = new Scanner(System.in);
+    
+    private String Ten;
+    private int Tuoi;
+    private String DiaChi;
+
+    public People(String Ten, int Tuoi, String DiaChi) {
+        this.Ten = Ten;
+        this.Tuoi = Tuoi;
+        this.DiaChi = DiaChi;
+    }
+    
+    public People(){
+        
+    }
+
+    public String getTen() {
+        return Ten;
+    }
+
+    public void setTen(String Ten) {
+        this.Ten = Ten;
+    }
+
+    public int getTuoi() {
+        return Tuoi;
+    }
+
+    public void setTuoi(int Tuoi) {
+        this.Tuoi = Tuoi;
+    }
+
+    public String getDiaChi() {
+        return DiaChi;
+    }
+
+    public void setDiaChi(String DiaChi) {
+        this.DiaChi = DiaChi;
+    }
+
+    @Override
+    public String toString() {
+        return "People{" + "Ten=" + Ten + ", Tuoi=" + Tuoi + ", DiaChi=" + DiaChi + '}';
+    }
+    
+    public void input(){
+        System.out.print("- Nhập tên: ");
+        this.Ten = z.nextLine();
+        System.out.print("- Nhập tuổi: ");
+        this.Tuoi = z.nextInt();
+        z.nextLine();
+        System.out.print("- Nhập địa chỉ: ");
+        this.DiaChi = z.nextLine();
+    }
+    
+    @Override
+    public String showInfor(){
+        return this.toString();
+    }
+}
---
+++ users/LeQuocTuan/assignment60Phut/Test.java
@@ -0,0 +1,85 @@
+﻿/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package APTECH.assignment60Phut;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author LQT
+ */
+public class Test {
+
+    static ArrayList<IInfor> a = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner z = new Scanner(System.in);
+        int Choose;
+        String Exit;
+        boolean Rep = false;
+
+        while (true) {
+            ShowMenu();
+
+            Choose = z.nextInt();
+
+            switch (Choose) {
+                case 1:
+                    System.out.println("");
+                    People People = new People();
+                    People.input();
+                    a.add(People);
+                    System.out.println("Nhập thông tin của đối tượng " + People.getTen() + " thành công!");
+                    break;
+                case 2:
+                    System.out.println("");
+                    Car Car = new Car();
+                    Car.input();
+                    a.add(Car);
+                    System.out.println("Nhập thông tin của đối tượng " + Car.getTen() + " thành công!");
+                    break;
+                case 3:
+                    System.out.println("");
+                    System.out.println("HIỂN THỊ THÔNG TIN");
+                    a.forEach((e)->{
+                        System.out.println(e.showInfor());
+                    });
+                    break;
+                case 4:
+                    System.out.println("");
+                    z.nextLine();
+                    do {
+                        System.out.print("Bạn có chắc chắn muốn thoát chương trình? (y/n): ");
+                        Exit = z.nextLine();
+                        if (Exit.length() == 1) {
+                            if (Exit.equalsIgnoreCase("y")) {
+                                System.exit(0);
+                            } else if (Exit.equalsIgnoreCase("n")) {
+                                Rep = false;
+                            } else {
+                                Rep = true;
+                            }
+                        }
+                        else{
+                            Rep = true;
+                        }
+                    } while (Rep == true);
+                    break;
+            }
+        }
+    }
+
+    public static void ShowMenu() {
+        System.out.println("");
+        System.out.println("NHẬP THÔNG TIN");
+        System.out.println("1. Nhập NGƯỜI");
+        System.out.println("2. Nhập XE");
+        System.out.println("3. Hiển thị thông tin tất cả đối tượng");
+        System.out.println("4. Thoát chương trình");
+        System.out.print("Mời bạn chọn: ");
+    }
+}
