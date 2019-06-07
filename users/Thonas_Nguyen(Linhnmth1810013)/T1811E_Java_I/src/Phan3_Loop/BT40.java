4e7de46ada6ef946738f956533f705d6c29a15e5
LexNguyen <linhnguyenmanh2681993@gmail.com>
2019-06-07 18:40:42 +0700

    update

---
+++ users/Thonas_Nguyen(Linhnmth1810013)/T1811E_Java_I/src/Phan3_Loop/BT40.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Phan3_Loop;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author linhn
+ */
+public class BT40 {
+    public static void main(String[] args) {
+        int p;
+        Scanner input = new Scanner(System.in);
+    }
+ 
+}
---
+++ users/Thonas_Nguyen(Linhnmth1810013)/T1811E_Java_I/src/Phan3_Loop/BT40_student_obj.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Phan3_Loop;
+
+/**
+ *
+ * @author linhn
+ */
+public class BT40_student_obj {
+    String fullName;
+    int age;
+    String address;
+    String email;
+    int phone;
+}
